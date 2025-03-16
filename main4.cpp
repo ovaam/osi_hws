47cdbcd34e4628127847a0e21aa409f6b052461d
ovaam <oamalova@edu.hse.ru>
2025-03-16 18:29:15 +0300

    hw for mark 4

--- main4.cpp
+++ main4.cpp
@@ -1,11 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 128
 
+// Функция для удаления дубликатов символов в строке
 void remove_duplicates(char *str) {
   int len = strlen(str);
   int index = 0;
@@ -24,6 +27,7 @@ void remove_duplicates(char *str) {
   str[index] = '\0';
 }
 
+// Функция для нахождения символов, которые есть в первой строке, но отсутствуют во второй
 void find_unique_chars(const char *str1, const char *str2, char *result) {
   int index = 0;
   for (int i = 0; str1[i] != '\0'; i++) {
@@ -37,20 +41,25 @@ void find_unique_chars(const char *str1, const char *str2, char *result) {
 
 int main(int argc, char *argv[]) {
   if (argc != 5) {
-    printf("Использование: %s <входной файл 1> <входной файл 2> <выходной файл 1> <выходной файл 2>\n", argv[0]);
-    return 1;
+    fprintf(stderr, "Использование: %s <входной файл 1> <входной файл 2> <выходной файл 1> <выходной файл 2>\n", argv[0]);
+    exit(EXIT_FAILURE);
   }
+
+  const char *input_file1 = argv[1];
+  const char *input_file2 = argv[2];
+  const char *output_file1 = argv[3];
+  const char *output_file2 = argv[4];
 
   int pipe1[2], pipe2[2];
   if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
     perror("Ошибка создания канала");
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   pid_t pid = fork();
   if (pid < 0) {
     perror("Ошибка создания процесса");
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   if (pid == 0) { // Процесс 2: обработка данных
@@ -70,12 +79,12 @@ int main(int argc, char *argv[]) {
 
     close(pipe1[0]);
     close(pipe2[1]);
-    exit(0);
+    exit(EXIT_SUCCESS);
   } else {
     pid_t pid2 = fork();
     if (pid2 < 0) {
       perror("Ошибка создания процесса");
-      return 1;
+      exit(EXIT_FAILURE);
     }
 
     if (pid2 == 0) { // Процесс 3: запись результата
@@ -87,11 +96,11 @@ int main(int argc, char *argv[]) {
       read(pipe2[0], result1, BUFFER_SIZE);
       read(pipe2[0], result2, BUFFER_SIZE);
 
-      int output1_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-      int output2_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      int output1_fd = open(output_file1, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      int output2_fd = open(output_file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
       if (output1_fd < 0 || output2_fd < 0) {
         perror("Ошибка открытия выходного файла");
-        return 1;
+        exit(EXIT_FAILURE);
       }
 
       write(output1_fd, result1, strlen(result1));
@@ -100,17 +109,17 @@ int main(int argc, char *argv[]) {
       close(output1_fd);
       close(output2_fd);
       close(pipe2[0]);
-      exit(0);
+      exit(EXIT_SUCCESS);
     } else { // Процесс 1: чтение данных
       close(pipe1[0]); // Закрываем чтение из первого канала
       close(pipe2[0]); // Закрываем чтение из второго канала
       close(pipe2[1]); // Закрываем запись во второй канал
 
-      int input1_fd = open(argv[1], O_RDONLY);
-      int input2_fd = open(argv[2], O_RDONLY);
+      int input1_fd = open(input_file1, O_RDONLY);
+      int input2_fd = open(input_file2, O_RDONLY);
       if (input1_fd < 0 || input2_fd < 0) {
         perror("Ошибка открытия входного файла");
-        return 1;
+        exit(EXIT_FAILURE);
       }
 
       char str1[BUFFER_SIZE], str2[BUFFER_SIZE];
